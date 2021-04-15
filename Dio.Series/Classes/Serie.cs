370d6bbafe8278db63e349058def6d7e8eded522
Jean Carlos <carlosjean090@gmail.com>
2021-04-15 14:22:38 -0300

    Adicionar arquivos de projeto.

---
+++ Dio.Series/Classes/Serie.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Dio.Series
+{
+    public class Serie : EntidadeBase
+    {
+        //Atributos
+
+        private Genero _genero { get; set; }
+
+        private string _titulo { get; set; }
+
+        private string _descricao { get; set; }
+
+        private int _ano { get; set; }
+
+        private bool _excluido { get; set; }
+
+        public Serie(int id, Genero genero, string titulo, string descricao, int ano)
+        {
+            Id = id;
+            _genero = genero;
+            _titulo = titulo;
+            _descricao = descricao;
+            _ano = ano;
+            _excluido = false;
+        }
+
+        public override string ToString()
+        {
+            string retorno = " ";
+            retorno += "Gênero: " + _genero + Environment.NewLine;
+            retorno += "Título: " + _titulo + Environment.NewLine;
+            retorno += "Descrição: " + _descricao + Environment.NewLine;
+            retorno += "Ano de Início: " + _ano;
+            retorno += " Excluída: " + _excluido;
+            return retorno;
+        }
+
+        public string RetornaTitulo()
+        {
+            return _titulo;
+        }
+
+        public int RetornaId()
+        {
+            return Id;
+        }
+
+        public bool RetornaExcluido()
+        {
+            return _excluido;
+        }
+
+        public void Excluir()
+        {
+            _excluido = true;
+        }
+    }
+}
